abe4ef3650b15e2fc2a0644c4bfe0563ee2e79da
Alfaroth <davide.valerio93@gmail.com>
2022-10-04 18:57:22 +0200

    Initial commit

---
+++ Lezione02/js/script.js
@@ -0,0 +1,8 @@
+// commento in js
+// da questo momento in poi scriveremo in js
+
+alert("Ciao, Davide!");
+// apre una finestra di dialogo con il messaggio all'apertura della pagina
+
+console.log("Ciao, mondo della console!");
+// scrive il messaggio nella console
