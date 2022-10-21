71e11a6b190e2a908a21fa24fa413ee645f4d2d5
Alfaroth <davide.valerio93@gmail.com>
2022-10-21 18:11:20 +0200

    Esercizio carousel

--- DarioLezione04-FunzioniMetodiOggetti/Esercizio01-Carousel/js/script01.js
+++ DarioLezione04-FunzioniMetodiOggetti/Esercizio01-Carousel/js/script01.js
@@ -1,8 +1,29 @@
 var immagini = [
-    "./img/Dino1.jpg",
-    "./img/Dino2.jpg",
-    "..."
+    "./img/pkmn01.png",
+    "./img/pkmn02.png",
+    "./img/pkmn03.png",
+    "./img/pkmn04.png",
+    "./img/pkmn05.png",
+    "./img/pkmn06.png",
+    "./img/pkmn07.png",
+    "./img/pkmn08.png",
+    "./img/pkmn09.png",
+    "./img/pkmn10.png"
 ];
 
 var elImmagine = document.getElementById("immagine");
-elImmagine.innerHTML = "<img id='immagineDino' src='" + immagini[0] + "'>";
+elImmagine.innerHTML = "<img id='immaginePkmn' src='" + immagini[0] + "'>";
+
+var i = 0;
+console.log(i);
+
+function ruotaFoto() {
+    if (i<(immagini.length-1)) {
+        elImmagine.innerHTML = "<img id='immaginePkmn' src='" + immagini[++i] + "'>";
+        console.log(i);
+    } else {
+        i=0;
+        elImmagine.innerHTML = "<img id='immaginePkmn' src='" + immagini[0] + "'>";
+        console.log(i);
+    }
+}
